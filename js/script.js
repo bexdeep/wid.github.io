325c65c7a954411d09ffa3e07c09fdbc38e655a2
bexdeep <bexdeep@gmail.com>
2021-05-16 10:55:51 +0930

    Updated Animation

--- js/script.js
+++ js/script.js
@@ -1,6 +1,23 @@
-const icon = document.querySelectorAll("#linkedin-logo path");
-console.log('Executing JS');
-console.log(icon.length);
-for (i = 0; i < icon.length; i++) {
-    console.log(`${i} is ${i}.getTotalLength()`);
-}
+window.onload = function () {
+    const button = document.getElementById("ctaButton");
+    button.onmouseover = () => {
+        console.log(button.className);
+        if (button.ClassName == "button buttonvisible") {
+            button.classList.remove("buttonvisible");
+        }
+
+    }
+};
+
+window.addEventListener('scroll', () => {
+    const button = document.getElementById("ctaButton");
+    const windowHeight = window.innerHeight;
+    var buttonTop = button.getBoundingClientRect().top;
+    var checkVisible = ((buttonTop - windowHeight + 25) < 0 && buttonTop > 0) ? 1 : 0;
+    if (checkVisible) {
+        if (button.ClassName != "button buttonvisible") button.classList.add("buttonvisible");
+    } else {
+        button.className = "button";
+    }
+})
+
