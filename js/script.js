52cf89197d4f9f49751eb27c203913b43eb388b0
bexdeep <bexdeep@gmail.com>
2021-05-18 21:29:08 +0930

    refactoring code

--- js/script.js
+++ js/script.js
@@ -1,12 +1,10 @@
 window.addEventListener('scroll', () => {
     const button = document.getElementById("ctaButton");
-    const windowHeight = window.innerHeight;
     var buttonTop = button.getBoundingClientRect().top;
-    var checkVisible = ((buttonTop - windowHeight + 25) < 0 && buttonTop > 0) ? 1 : 0;
-    if (checkVisible) {
-        if (button.ClassName != "button buttonvisible") button.classList.add("buttonvisible");
+    var buttonIsVisible = (buttonTop - window.innerHeight + 25) < 0 && buttonTop > 0;
+    if (buttonIsVisible) {
+        if (button.ClassName != "button animateButton") button.classList.add("animateButton");
     } else {
         button.className = "button";
     }
 })
-
