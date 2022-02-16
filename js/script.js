f113bf3fe46f2f171936ab8f867e679dcbafb67a
Deep @BigDataCloud <deep@bigdatacloud.com>
2022-02-16 16:41:34 +1030

    New Content

--- js/script.js
+++ js/script.js
@@ -1,39 +1,34 @@
 
 const button = document.getElementById("ctaButton");
-button.onmouseover = () => buttonMouseOver();
-button.onmouseout = () => buttonMouseOut();
 
 function buttonMouseOver() {
     if (!button.classList.contains("animateButton")) button.classList.add("animateButton");
     else {
-        button.classList.remove("animateButton")
-        setTimeout(() => { button.classList.add("animateButton") }, 100)
-    }
-}
-function buttonMouseOut() {
-    if (button.classList.contains("animateButton")) {
         button.classList.remove("animateButton");
-        button.classList.add("hasAnimated");
-    }
-}
-
-window.onload = () => {
-    var buttonTop = button.getBoundingClientRect().top;
-    var buttonIsVisible = (buttonTop - window.innerHeight + 25) < 0 && buttonTop > 0;
-    if (buttonIsVisible && button.ClassName != "button animateButton") {
+        button.offsetWidth = button.offsetWidth;
         button.classList.add("animateButton");
     }
 }
 
+function isVisible(el) {
+    var elTop = el.getBoundingClientRect().top;
+    return (elTop - window.innerHeight + 25) < 0 && elTop > 0;
+}
+
+
+window.onload = () => {
+    if (isVisible(button)) button.classList.add("animateButton");
+}
+
 window.addEventListener('scroll', () => {
-    console.log(button.className);
-    var buttonTop = button.getBoundingClientRect().top;
-    var buttonIsVisible = (buttonTop - window.innerHeight + 25) < 0 && buttonTop > 0;
-    if (buttonIsVisible) {
-        if (!button.classList.contains("animateButton") && !button.classList.contains("hasAnimated")) {
+    if (isVisible(button)) {
+        if (!button.classList.contains("animateButton")) {
             button.classList.add("animateButton");
         }
     } else {
         button.className = "button";
     }
 })
+
+button.addEventListener('mouseenter', buttonMouseOver);
+
